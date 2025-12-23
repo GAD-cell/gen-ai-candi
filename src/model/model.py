d5730f035b4e5615e89ba5c20008f7e6dedfa6b4
GAD-cell <gadsinoue@gmail.com>
2025-12-23 15:05:46 +0100

    working inference

--- src/model/model.py
+++ src/model/model.py
@@ -19,7 +19,7 @@ llama_config = LlamaConfig(
 )
 
 class CandiLlama(nn.Module):
-    def __init__(self, llama_config):
+    def __init__(self, llama_config, device):
         super().__init__()
         self.llama = LlamaForCausalLM(llama_config)
         self.llama = ReformatModelForDiff(self.llama).get_model()
@@ -30,22 +30,43 @@ class CandiLlama(nn.Module):
         self.r_max = 0.499
         self.r_min = 1e-5
         self.corruption_lambd = 0.5
+
+        self.device = device
+
+    def _get_sigma(self,t):
+        r_t = (self.r_max - self.r_min) * t + self.r_min
+        r_t_np = r_t.cpu().numpy()
+        inv_phi = torch.from_numpy(norm.ppf(r_t_np)).to(self.device).float()
+        sigma_t = -1 / (inv_phi * torch.sqrt(torch.tensor(2.0)))
+        
+        return sigma_t
     
-    def _get_kernel_noising(self, input_ids, attention_mask):
+    def _get_noising_params(self, t, bsz, seq_len, attention_mask=None):
+
+        if len(t.shape)==1:
+            t = t.repeat(bsz,1)
+        
+        alpha_t = 1 - t
+        
+        sigma_t = self._get_sigma(t)
+
+
+        m_t = torch.bernoulli(alpha_t.expand(bsz, seq_len))
+        if attention_mask is not None :
+            m_t = torch.where(attention_mask == 0, torch.ones_like(m_t), m_t)
+  
+        return alpha_t, sigma_t, m_t
+
+
+    def _get_noising_kernel(self, input_ids, attention_mask):
         bsz, seq_len = input_ids.shape
         device = input_ids.device
 
+
         t = torch.rand(bsz, 1, device=device)
-        alpha_t = 1 - t
         
-        r_t = (self.r_max - self.r_min) * t + self.r_min
-        r_t_np = r_t.cpu().numpy()
-        inv_phi = torch.from_numpy(norm.ppf(r_t_np)).to(device).float()
-        sigma_t = -1 / (inv_phi * torch.sqrt(torch.tensor(2.0)))
-
-        m_t = torch.bernoulli(alpha_t.expand(bsz, seq_len))
-        m_t = torch.where(attention_mask == 0, torch.ones_like(m_t), m_t)
-
+        alpha_t, sigma_t, m_t = self._get_noising_params(t, bsz, seq_len, attention_mask)
+        
         x_0_one_hot = F.one_hot(input_ids, num_classes=self.vocab_size).float() 
         noise = torch.randn_like(x_0_one_hot)
         sigma_mask = attention_mask.unsqueeze(-1).float()
@@ -56,7 +77,7 @@ class CandiLlama(nn.Module):
     def forward(self, input_ids, attention_mask, m_t=None, labels=None):
 
         if self.training:
-            alpha_t, m_t, x_tilde_one_hot = self._get_kernel_noising(input_ids, attention_mask)
+            alpha_t, m_t, x_tilde_one_hot = self._get_noising_kernel(input_ids, attention_mask)
             
             W_embed = self.llama.get_input_embeddings().weight
             x_tilde_embed = torch.matmul(x_tilde_one_hot, W_embed)
@@ -69,10 +90,10 @@ class CandiLlama(nn.Module):
                 attention_mask=attention_mask,
                 return_dict=True
             )
+
             if labels is not None :
                 log_probs = F.log_softmax(outputs.logits, dim=-1)
                 target_log_probs = log_probs.gather(dim=-1, index=labels.unsqueeze(-1)).squeeze(-1)
-                print(target_log_probs.shape)
                 outputs.loss = (-target_log_probs/ (1.0 - alpha_t)).mean()
             return outputs  
                      
@@ -83,5 +104,49 @@ class CandiLlama(nn.Module):
                 return_dict=True
                 )
             return outputs
+    
+    def generate(self, inputs_ids=None, attention_mask=None, nfe=8, generation_length=128, batch_size=1):
+        
+        t = torch.Tensor([1.0])
+        alpha_t, sigma_t, m_t = self._get_noising_params(t, batch_size, generation_length,)
+        noise = torch.randn((batch_size,generation_length,self.vocab_size))
+        x_tilde_one_hot = sigma_t.unsqueeze(-1) * noise
+        # Initial predictions
+        x_t = torch.argmax(x_tilde_one_hot,dim=-1)
+
+        W_embed = self.llama.get_input_embeddings().weight
+        # Initial noisy embeddings
+        y_cache = (1 - self.corruption_lambd ) * torch.matmul(x_tilde_one_hot, W_embed) + self.corruption_lambd  * self.corruption_bias 
+
+        t_steps = torch.arange(1,-1/nfe,-1/nfe)
+        for t in t_steps :
+            y_t = torch.where(m_t.unsqueeze(-1)==1, self.llama.get_input_embeddings()(x_t), y_cache)
+            # x_s_new from learned posterior 
+            x_s_new = self.llama(
+                inputs_embeds=y_t,
+                attention_mask=attention_mask,
+                return_dict=True
+            ).logits
+
+            # mask randomly to create anchor points
+            u = torch.rand((batch_size,generation_length))
+            alpha_s = alpha_t - 1/nfe
+            m_s_new = u < (alpha_s - alpha_t) / (1 - alpha_t)
+            
+            # score function
+            x_hat_s = torch.argmax(x_s_new,dim=-1)
+            grad_logp = - (y_t - self.llama.get_input_embeddings()(x_hat_s)) / (sigma_t)**2
+
+            # ODE update
+            s = (t-1/nfe).repeat(batch_size,1)
+            sigma_s = self._get_sigma(s)
+            y_cache = y_t - (sigma_t - sigma_s) * grad_logp
+
+            m_s_prime = (m_s_new) & (m_t == 0)
+            x_t = torch.where(m_t == 1, x_t, torch.where(m_s_prime, x_hat_s, x_t))
+            m_t = torch.logical_or(m_t, m_s_new).float()
+
+
+        return x_t
     
 
--- src/test_model.py
+++ src/test_model.py
@@ -1,12 +1,21 @@
 from src.model.model import CandiLlama, llama_config
 import torch
+from transformers import GPT2Tokenizer
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model = CandiLlama(llama_config).to(device)
+model = CandiLlama(llama_config,  device=device).to(device)
 model.train()
 
 dummy_input = torch.randint(0, 50257, (2, 128)).to(device)
 dummy_mask = torch.ones_like(dummy_input)
 
 outputs = model(dummy_input, attention_mask=dummy_mask, labels=dummy_input)
-print(f"Test Forward réussi. Shape des logits : {outputs.logits.shape}")
+print(f"Test Forward. output shape : {outputs.logits.shape}")
+
+# generation 
+outputs_ids = model.generate()
+print(f"Test Generation. output : {outputs}")
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+text = tokenizer.batch_decode(outputs_ids,skip_special_tokens=True)
+print(text)
